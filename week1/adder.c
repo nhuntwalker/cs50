9288a8b6e214f0c53f72b3f5fd7a59c43d9a37c2
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2017-08-18 16:02:43 -0700

    Created the adder program

---
+++ week1/adder.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("x is ");
+    int x;
+    scanf("%d", &x);
+
+    printf("y is ");
+    int y;
+    scanf("%d", &y);
+
+    printf("sum of x and y is %i\n", x + y);
+}
---
+++ week1/int.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+    int i;
+    printf("Write a number: ");
+    scanf("%d", &i);
+    printf("%d + 1 = %d\n", i, i + 1);
+
+}
